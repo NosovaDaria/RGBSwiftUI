288637f3d322628d3da401d8d188dd84f114d951
NosovaDaria <kuleshovadasha@gmail.com>
2022-02-20 14:11:40 +0300

    Initial Commit

---
+++ RGBSwiftUI/RGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSwiftUIApp.swift
+//  RGBSwiftUI
+//
+//  Created by Дарья Носова on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
